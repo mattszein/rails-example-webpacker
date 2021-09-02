bdefd93b6655d6e7ef3c4a21605ef496bc9ddf52
Matt Szein} <mattszeindev@gmail.com>
2021-09-02 13:28:19 -0300

    add configuration for system test using cuprite, and change docker-compose for rspec_system service

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
@@ -5,25 +7,7 @@ require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
-# Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
-
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -61,6 +45,26 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Add `travel_to`
+  # See https://andycroll.com/ruby/replace-timecop-with-rails-time-helpers-in-rspec/
+  config.include ActiveSupport::Testing::TimeHelpers
+
+  config.after do
+    # Make sure every example starts with the current time
+    travel_back
+
+    # Clear ActiveJob jobs
+    if defined?(ActiveJob) && ActiveJob::QueueAdapters::TestAdapter === ActiveJob::Base.queue_adapter
+      ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+      ActiveJob::Base.queue_adapter.performed_jobs.clear
+    end
+
+    if defined?(ActionMailer)
+      # Clear deliveries
+      ActionMailer::Base.deliveries.clear
+    end
+  end
 end
 
 Shoulda::Matchers.configure do |config|
---
+++ spec/system/support/precompile_assets.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  # Skip assets precompilcation if we exclude system specs.
+  # For example, you can run all non-system tests via the following command:
+  #
+  #    rspec --tag ~type:system
+  #
+  # In this case, we don't need to precompile assets.
+  next if config.filter.opposite.rules[:type] == "system" || config.exclude_pattern.match?(%r{spec/system})
+
+  config.before(:suite) do
+    # We can use webpack-dev-server for tests, too!
+    # Useful if you working on a frontend code fixes and want to verify them via system tests.
+    if Webpacker.dev_server.running?
+      $stdout.puts "\n⚙️  Webpack dev server is running! Skip assets compilation.\n"
+      next
+    else
+      $stdout.puts "\n🐢  Precompiling assets.\n"
+
+      # The code to run webpacker:compile Rake task
+      # ...
+    end
+  end
+end
